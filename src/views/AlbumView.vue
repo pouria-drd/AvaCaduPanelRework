970917c36bfeccaa2cf99663a6eec5f754046df7
Pour1a-D <pour1a.dra80@gmail.com>
2023-07-24 12:30:28 +0330

    Album table completed

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -98,111 +98,116 @@ export default {
 
 <template>
     <main class="bg-ava-panel-bg-gray w-full">
-        <div class="flex flex-col rounded-ava10 overflow-x-auto h-[60vh] overflow-y-auto">
-            <table class="">
-                <thead class="bg-ava-th-bg font-yekanX text-center text-ava-gray text-xs sticky top-0">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            عملیات
-                        </th>
+        <div class="flex flex-col h-[80vh]">
+            <div class="p-3 flex justify-end bg-white rounded-t-ava10">
+                <p class="text-ava-black my-auto font-bjn text-xl font-semibold">
+                    لیست آلبوم ها
+                </p>
+            </div>
+            <div class="flex flex-col overflow-x-auto overflow-y-auto bg-white">
+                <table class="bg-white">
+                    <thead class="bg-ava-th-bg font-yekanX text-center text-ava-gray text-xs sticky top-0">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                عملیات
+                            </th>
 
-                        <th scope="col" class="px-6 py-3">
-                            وضعیت
-                        </th>
+                            <th scope="col" class="px-6 py-3">
+                                وضعیت
+                            </th>
 
-                        <th scope="col" class="px-6 py-3">
-                            شماره تماس
-                        </th>
+                            <th scope="col" class="px-6 py-3">
+                                شماره تماس
+                            </th>
 
-                        <th scope="col" class="px-6 py-3">
-                            شناسه
-                        </th>
+                            <th scope="col" class="px-6 py-3">
+                                شناسه
+                            </th>
 
-                        <th scope="col" class="px-6 py-3">
-                            تاریخ ثبت
-                        </th>
-                    </tr>
-                </thead>
-                <tbody v-if="!isGettingData" class="text-center">
-                    <tr v-for="album in lazyAlbum" :key="album.id"
-                        class="bg-white border-b border-ava-border-bg font-yekanX text-sm text-ava-gray">
+                            <th scope="col" class="px-6 py-3">
+                                تاریخ ثبت
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="!isGettingData" class="text-center">
+                        <tr v-for="album in lazyAlbum" :key="album.id"
+                            class="bg-white border-b border-ava-border-bg font-yekanX text-sm text-ava-gray">
 
-                        <td class="h-full">
-                            <qr-icon class="cursor-pointer mx-auto hover:text-black transition"
-                                @click="OpenQRModal(album.uniqueKey)" />
-                        </td>
+                            <td class="h-full">
+                                <qr-icon class="cursor-pointer mx-auto hover:text-black transition"
+                                    @click="OpenQRModal(album.uniqueKey)" />
+                            </td>
 
-                        <td class="h-full">
-                            <div v-if="album.isActive"
-                                class="bg-ava-table-bg-green text-ava-success rounded-ava18 h-8 w-20 mx-auto">
-                                <p class="font-yekanX text-xs leading-8">
-                                    فعال
-                                </p>
-                            </div>
-
-                            <div v-else class="bg-ava-info-bg-red text-ava-orange rounded-ava18 h-8 w-20 mx-auto">
-                                <p class="font-yekanX text-xs leading-8">
-                                    غیر فعال
-                                </p>
-                            </div>
-                        </td>
-
-                        <td class="px-6 py-4">
-                            {{ album.phoneNumber }}
-                        </td>
-
-                        <td class="px-6 py-4">
-                            {{ album.contractGuid }}
-                        </td>
-
-                        <td class="ss02 px-0 py-4 block">
-                            <div class="flex my-auto items-center justify-center flex-col content-center">
-                                <p class="font-yekanX m-0 text-ava-black">
-                                    {{ ConvertToPersianDate(album.createdAt).date }}
-                                </p>
-
-                                <p class="font-yekanX m-0 text-xs text-ava-gray">
-                                    {{ ConvertToPersianDate(album.createdAt).time }}
-                                </p>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-                <tfoot class="text-center">
-                    <tr>
-                        <td class="bg-white">
-
-                        </td>
-                        <td class="bg-white">
-
-                        </td>
-                        <td class="bg-white">
-                            <div v-if="isGettingData" class="bg-white flex items-center justify-center">
-                                <p class="mt-3 text-ava-green font-yekanX text-sm">
-                                    درحال دریافت
-
-                                </p>
-
-                                <div class="ml-2 spinner-border spinner-border-sm text-ava-green" role="status">
+                            <td class="h-full">
+                                <div v-if="album.isActive"
+                                    class="bg-ava-table-bg-green text-ava-success rounded-ava18 h-8 w-20 mx-auto">
+                                    <p class="font-yekanX text-xs leading-8">
+                                        فعال
+                                    </p>
                                 </div>
-                            </div>
 
-                            <div v-if="!isGettingData && showRequestDataBtn"
-                                class="bg-white flex items-center justify-center">
-                                <p @click="RequestMoreData" class="mt-3 text-ava-green font-yekanX text-sm cursor-pointer">
-                                    نمایش بیشتر
-                                </p>
-                            </div>
-                        </td>
-                        <td class="bg-white">
+                                <div v-else class="bg-ava-info-bg-red text-ava-orange rounded-ava18 h-8 w-20 mx-auto">
+                                    <p class="font-yekanX text-xs leading-8">
+                                        غیر فعال
+                                    </p>
+                                </div>
+                            </td>
 
-                        </td>
-                        <td class="bg-white">
+                            <td class="px-6 py-4">
+                                {{ album.phoneNumber }}
+                            </td>
 
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
+                            <td class="px-6 py-4">
+                                {{ album.contractGuid }}
+                            </td>
+
+                            <td class="ss02 px-0 py-4 block">
+                                <div class="flex my-auto items-center justify-center flex-col content-center">
+                                    <p class="font-yekanX m-0 text-ava-black">
+                                        {{ ConvertToPersianDate(album.createdAt).date }}
+                                    </p>
+
+                                    <p class="font-yekanX m-0 text-xs text-ava-gray">
+                                        {{ ConvertToPersianDate(album.createdAt).time }}
+                                    </p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot class="text-center">
+                        <tr>
+                            <td class="bg-white">
+
+                            </td>
+                            <td class="bg-white">
+
+                            </td>
+                            <td class="bg-white">
+                                <div v-if="isGettingData" class="bg-white flex items-center justify-center">
+                                    <p class="mt-3 text-ava-green font-yekanX text-sm">
+                                        درحال دریافت
+
+                                    </p>
+
+                                    <div class="ml-2 spinner-border spinner-border-sm text-ava-green" role="status">
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="bg-white">
+
+                            </td>
+                            <td class="bg-white">
+
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <div v-if="!isGettingData && showRequestDataBtn" class="flex justify-center bg-white rounded-b-ava10">
+                <p @click="RequestMoreData" class="mt-3 text-ava-green font-yekanX text-sm cursor-pointer">
+                    نمایش بیشتر
+                </p>
+            </div>
         </div>
     </main>
 </template>
